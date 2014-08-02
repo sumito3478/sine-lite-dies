61094f6b846e4528e868472dc3dc248d3199ceb1
sumito3478 <sumito3478@gmail.com>
2014-08-02 19:54:43 +0900

    Fix a bug of Git.lastModifiedTime

--- src/Git.scala
+++ src/Git.scala
@@ -7,5 +7,6 @@ import java.time.format._
 
 object Git {
   def lastModifiedTime(path: Option[Path] = None): Option[ZonedDateTime] =
-    Seq("git", "log", "-1", "--follow", "--date=iso", "--pretty=format:%ad", path.toString).lineStream.dropWhile(_.isEmpty).headOption.map(ZonedDateTime.parse(_, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z")))
+    (Seq("git", "log", "-1", "--date=iso", "--pretty=format:%ad") ++ path.toSeq.flatMap(x => Seq("--follow", x.toString)))
+      .lineStream.dropWhile(_.isEmpty).headOption.map(ZonedDateTime.parse(_, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z")))
 }
