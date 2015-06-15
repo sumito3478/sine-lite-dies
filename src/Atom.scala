1336a2626a818eb80751d68ee1a39c9bb7907ea8
sumito3478 <sumito3478@gmail.com>
2015-06-15 17:33:40 +0900

    feat(clean): remove the old generator written in Scala

--- src/Atom.scala
+++
@@ -1,8 +0,0 @@
-package sine.lite.dies
-
-import java.security._
-import org.apache.commons.codec.binary.Base32
-
-object Atom {
-  def stringToURN(x: String) = s"urn:sha1:${new Base32().encodeToString(MessageDigest.getInstance("SHA-1").digest(x.getBytes("UTF-8")))}"
-}
--- src/Content.scala
+++
@@ -1,6 +0,0 @@
-package sine.lite.dies
-
-import java.time.ZonedDateTime
-import org.eclipse.jgit.lib._
-
-case class Content(body: String, metaData: Map[String, String], tags: List[String], title: String, permalink: String, updatedAt: ZonedDateTime, createdAt: ZonedDateTime, urn: String)
